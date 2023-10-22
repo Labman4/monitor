ed14956b8d245c31a0ce359bede9e14572981978
Labman4 <50023562+Labman4@users.noreply.github.com>
2023-10-22 15:10:43 +0800

    clear

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"net"
 	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,21 +9,9 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		clientIP := getClientIP(c.Request)
-		c.JSON(http.StatusOK, gin.H{"client_ip": clientIP})
+		clientIP := c.ClientIP()
+		c.String(http.StatusOK, clientIP)
 	})
 
 	r.Run(":11415")
-}
-
-func getClientIP(req *http.Request) string {
-	clientIP := req.Header.Get("X-Real-IP") 
-	if clientIP == "" {
-		clientIP = req.Header.Get("X-Forwarded-For")
-	}
-	if clientIP == "" {
-		clientIP = req.RemoteAddr
-	}
-	host, _, _ := net.SplitHostPort(clientIP)
-	return host
 }
